cf961b24370b88dc41ccc92a07ff8ecd47d3298c
Amr Sherif <amrshrif@stanford.edu>
2024-09-13 12:21:34 +0400

    Update pyproject.toml, setup.cfg, __init__.py [skip ci]
    Signed-off-by: Amr Sherif <amrshrif@stanford.edu>

--- src/bi_toolbox/__init__.py
+++ src/bi_toolbox/__init__.py
@@ -1,9 +1,6 @@
-# Define package version
 __version__ = "0.1.0a1"
 
-# Import key submodules or classes from the package
 from .utils import CredManager, CredManagerConfig
 from .Reporting import GenericReportProcessor
 
-# Define the public API for the package
 __all__ = ["CredManager", "CredManagerConfig", "GenericReportProcessor"]
